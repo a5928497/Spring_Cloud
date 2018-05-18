d2b754d36d3565c3b8ed54550baae04cf582c39b
a5928497 <61722615@qq.com>
2018-05-18 21:50:13 +0800

    extends Eureka

--- microserviceprovider8001/src/main/java/com/lzl/Provider8001_APP.java
+++ microserviceprovider8001/src/main/java/com/lzl/Provider8001_APP.java
@@ -2,8 +2,10 @@ package com.lzl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Provider8001_APP {
     public static void main(String[] args) {
         SpringApplication.run(Provider8001_APP.class,args);
