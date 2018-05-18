7c4e3ee9c2dacffb91932e6107372533eb29009a
a5928497 <61722615@qq.com>
2018-05-18 10:16:31 +0800

    create first module

---
+++ microserviceapi/src/main/java/com/lzl/entity/Dept.java
@@ -0,0 +1,18 @@
+package com.lzl.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Accessors(chain = true)
+public class Dept implements Serializable{
+    private  int id;
+    private String deptName;
+    private String dbSource;
+}
