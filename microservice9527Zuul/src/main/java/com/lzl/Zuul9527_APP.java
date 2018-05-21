5b841d41eff99dedebc14ffaf224a561724f2bba
a5928497 <61722615@qq.com>
2018-05-22 00:43:46 +0800

    add Zuul

---
+++ microservice9527Zuul/src/main/java/com/lzl/Zuul9527_APP.java
@@ -0,0 +1,14 @@
+package com.lzl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class Zuul9527_APP {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zuul9527_APP.class,args);
+    }
+}
