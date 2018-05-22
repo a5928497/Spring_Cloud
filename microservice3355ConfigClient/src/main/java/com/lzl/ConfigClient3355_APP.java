fabd14513647c3791f9a3ba922e5ac40ae2bb4d5
a5928497 <61722615@qq.com>
2018-05-23 00:11:33 +0800

    add config client

---
+++ microservice3355ConfigClient/src/main/java/com/lzl/ConfigClient3355_APP.java
@@ -0,0 +1,11 @@
+package com.lzl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClient3355_APP {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient3355_APP.class,args);
+    }
+}
